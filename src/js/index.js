445fe656cf0ea3de9864a07f22271d621ce561b7
ridwanndwip <ridwanndwip@gmail.com>
2023-06-04 14:25:48 +0700

    add FAQ and Gallery

--- src/js/index.js
+++ src/js/index.js
@@ -23,3 +23,16 @@ for(let i=0; i<indicatorImages.length; i++){
 
     })
 }
+
+let li = document.querySelectorAll(".faq-text li");
+    for (var i = 0; i < li.length; i++) {
+      li[i].addEventListener("click", (e)=>{
+        let clickedLi;
+        if(e.target.classList.contains("question-arrow")){
+          clickedLi = e.target.parentElement;
+        }else{
+          clickedLi = e.target.parentElement.parentElement;
+        }
+       clickedLi.classList.toggle("showAnswer");
+      });
+    }
