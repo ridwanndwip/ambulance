3767990da7cc4365a32f6c285c7ffd7c1bb66c56
ridwanndwip <ridwanndwip@gmail.com>
2023-06-04 12:38:51 +0700

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,25 @@
+const menuToggle = document.querySelector('.nav_toggle input');
+const nav = document.querySelector('nav ul');
+const slides=document.querySelector(".testimonials_slider").children;
+const indicatorImages=document.querySelector(".slider-indicator").children;
+
+menuToggle.addEventListener('click', function () {
+    nav.classList.toggle('slide')
+});
+
+for(let i=0; i<indicatorImages.length; i++){
+    indicatorImages[i].addEventListener("click",function(){
+     
+       for(let j=0; j<indicatorImages.length; j++){
+         indicatorImages[j].classList.remove("active");
+       }
+        this.classList.add("active");
+        const id=this.getAttribute("data-id");
+       for(let j=0; j<slides.length; j++){
+           slides[j].classList.remove("active");
+       }
+
+        slides[id].classList.add("active");
+
+    })
+}
